d32ae23e3233f18bc1a00959e3ccee0dfe82cb47
martaue <marta.krymer@edu.uekat.pl>
2023-12-04 15:30:31 +0100

    dodano nowe zadanie

--- zad6.py
+++ zad6.py
@@ -4,7 +4,9 @@ def merge_and_cube_lists(list1, list2):
     cubed_values = [x**3 for x in unique_values]
     return cubed_values
 
+
 list_a = [1, 2, 3, 4, 5]
 list_b = [3, 4, 5, 6, 7]
 result_list = merge_and_cube_lists(list_a, list_b)
-print(f"Wynik po złączeniu list, usunięciu duplikatów i podniesieniu do potęgi 3: {result_list}")
+print(f"Wynik po złączeniu list, usunięciu duplikatów "
+      f"i podniesieniu do potęgi 3: {result_list}")
