7cb7cc249212daae33aeb771d936aac120b0b27d
martaue <marta.krymer@edu.uekat.pl>
2023-12-04 15:26:33 +0100

    dodano nowe zadanie

--- zad3.py
+++ zad3.py
@@ -1,7 +1,8 @@
 def czyparzysta(liczba: int):
     if liczba % 2 == 0:
-        return("liczba jest parzysta")
+        return ("liczba jest parzysta")
     else:
-        return("liczba jest nieparzysta")
+        return ("liczba jest nieparzysta")
+
 
 print(czyparzysta(24))
