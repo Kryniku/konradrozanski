a404a3a092d2a4da89bacf8cd0c627d7af2a38d8
Kryniku <147411757+Kryniku@users.noreply.github.com>
2023-12-04 13:45:24 +0100

    Add files via upload

---
+++ zad1.py
@@ -0,0 +1,13 @@
+class student:
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
+    def is_passed(self):
+        average_marks = sum(self.marks) / len(self.marks)
+        return average_marks > 50
+
+student1 = student("Maciuś",[40,60,70,80,20])
+student2 = student("Natalia",[10,20,30,20,20])
+
+print(f"{student1.name} zaliczył: {student1.is_passed()}")
+print(f"{student2.name} zaliczył: {student2.is_passed()}")
